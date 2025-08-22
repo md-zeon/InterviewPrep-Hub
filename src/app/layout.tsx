0de77e1cbb68f29084a3c3777f386cdfc1466a44
md-zeon <zeonrahaman5870@gmail.com>
2025-08-22 17:10:24 +0600

    Added Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import { ModeToggle } from "@/components/ModeToggle";
 import { Toaster } from "@/components/ui/sonner";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -37,7 +37,7 @@ export default function RootLayout({
 					enableSystem
 					disableTransitionOnChange
 				>
-					<ModeToggle />
+					<Navbar />
 					{children}
 				</ThemeProvider>
 				<Toaster />
